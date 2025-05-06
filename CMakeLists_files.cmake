aa896efdf734609d6b270566bb98c8cfa37de273
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2025-05-06 10:20:58 +0200

    added functions to manipulate a deckkeyword and deckitem.

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -809,6 +809,7 @@ if(ENABLE_ECL_INPUT)
     tests/BASE.UNRST
   )
   list (APPEND EXAMPLE_SOURCE_FILES
+    examples/manipulatedeck.cpp
     examples/opmi.cpp
     examples/opmpack.cpp
     examples/opmhash.cpp
@@ -826,6 +827,7 @@ list (APPEND PROGRAM_SOURCE_FILES
 )
 if(ENABLE_ECL_INPUT)
   list (APPEND PROGRAM_SOURCE_FILES
+    examples/manipulatedeck.cpp
     examples/opmi.cpp
     examples/opmpack.cpp
     examples/opmhash.cpp
